f1a6ea13e450712ccd1f9962d212ae845c47d258
Filipp <fill12fill82005@gmail.com>
2025-02-13 15:37:11 +0500

    end

--- Rectangles/main.cpp
+++ Rectangles/main.cpp
@@ -6,18 +6,8 @@ int main()
 {
     setlocale(LC_ALL, "ru");
 
-    /*interface ui;
-    ui.run();*/
-
-    std::vector<rectangle> rect = {
-        {{-1.5, 0.5}, {2, 1.5}},
-        {{-0.5, 0}, {0, 2.25}},
-        {{0, 0}, {1.5, 2.25}},
-        {{1, 2}, {4, 3}}
-    };
-
-    native al(rect, 1);
-    al.execute();
+    interface ui;
+    ui.run();
 
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     return 0;
